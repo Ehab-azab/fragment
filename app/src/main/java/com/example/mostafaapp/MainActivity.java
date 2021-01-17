0bdb031ff5544ab3578dab7f02e7d2567f3403c4
Ehab Azab <Ehab-Azab>
2021-01-17 20:12:21 +0200

    another way

--- app/src/main/java/com/example/mostafaapp/MainActivity.java
+++ app/src/main/java/com/example/mostafaapp/MainActivity.java
@@ -1,32 +1,52 @@
 package com.example.mostafaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-int number=0;
+
     TextView tv;
+    Button tsbeh;
+    int number2=0;
+    Myfragment fragment = new Myfragment();
+    Bundle bundle = new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FrameLayout fragment=(FrameLayout)findViewById(R.id.fragment);
+        tsbeh=(Button) findViewById(R.id.ac_btn);
          tv=findViewById(R.id.tv);
+tsbeh.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        trftrshData();
+    }
+});
 
-
+        bundle.putInt("number2", number2);
+        fragment.setArguments(bundle);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment, new Myfragment());
+        ft.replace(R.id.fragment, fragment);
         ft.commit();
     }
-    public void increasenumber(){
-        number+=1;
+    public void increasenumber(int number){
+
         Log.e("n",number+"");
         tv.setText( number+"");
+    }
+    public void trftrshData(){
+        number2+=1;
+        bundle.putInt("number2", number2);
+        fragment.setArguments(bundle);
+        fragment.fun();
     }
 }
--- app/src/main/java/com/example/mostafaapp/Myfragment.java
+++ app/src/main/java/com/example/mostafaapp/Myfragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,9 @@ import androidx.fragment.app.Fragment;
 // Created by Ehab Azab on 1/17/2021.
 //
 public class Myfragment extends Fragment {
+    int number=0;
+    int myInt;
+    TextView tvbox;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,14 +30,24 @@ public class Myfragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tvbox=(TextView) view.findViewById(R.id.f_tv);
         Button mybtn=(Button) view.findViewById(R.id.btn);
         mybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((MainActivity)getActivity()).increasenumber();
+                number+=1;
+                ((MainActivity)getActivity()).increasenumber( number);
             }
         });
 
+
+    }
+    public void fun(){
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            myInt = bundle.getInt("number2", 0);
+        }
+        tvbox.setText(myInt+"");
     }
 }
 
